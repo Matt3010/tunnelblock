708bca307ddc0009f89a346274df2b41200cf640
Matteo Scanferla <73390839+Matt3010@users.noreply.github.com>
2026-09-02 11:48:46 +0200

    feat: expose generic HTTPS strategy

--- https/app/strategies/__init__.py
+++ https/app/strategies/__init__.py
@@ -1,3 +1,4 @@
+from app.strategies.generic import GenericStrategy
 from app.strategies.instagram import InstagramStrategy
 
-__all__ = ["InstagramStrategy"]
+__all__ = ["GenericStrategy", "InstagramStrategy"]
